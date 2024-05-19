eef61774e9567e764579254664e6529a38ef6814
Mie2511 <vuthu09834@gmail.com>
2024-05-19 23:56:51 +0700

    second commit

--- socialnetworking/src/app/pages/post-feed/post-feed.component.ts
+++ socialnetworking/src/app/pages/post-feed/post-feed.component.ts
@@ -29,7 +29,7 @@ export class PostFeedComponent implements OnInit {
         path: ["Posts"],
         where:[
           new OrderBy("timestamp", "desc"),
-          new Limit(10)
+          new Limit(100)
         ],
         onComplete: (result) => {
           result.docs.forEach(
